8bbee72dc976ebb999362a1834a384621328104b
Anthony Barrier <barrier.anthony@gmail.com>
2020-02-20 19:21:37 +0100

    lib books parse

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python3
 
+from library import Library
+from book import Book
+
 def process(filein, fileout):
-    pass
+    lines = filein.readlines()
+    nb_books, nb_libs, days = map(int,lines[0].split(' '))
+    books = {}
+    book_scores = list(map(int,lines[1].split(' ')))
+    for i in range(nb_books):
+        books[i] = Book(i, book_scores[i], 0)
+    libs = []
+    lib_id = 0
+    for i in range(2,2+2*nb_libs,2):
+        capacity, signup, ship = map(int, lines[i].split(' '))
+        libbooks = list(map(lambda x: books[int(x)], lines[i +1].split(' ')))
+        libs.append(Library(lib_id, signup, ship, libbooks))
+        lib_id = lib_id + 1
 
 if __name__ == "__main__":
     import argparse
--- library.py
+++ library.py
@@ -1,7 +1,8 @@
 class Library:
-    def __init__(self, id, signup_days, books):
+    def __init__(self, id, signup_days, ship, books):
         self.id = id
         self.signup_days = signup_days
+        self.ship = ship
         self.unique_books = books
         self.score = 0
         self.is_open = True
