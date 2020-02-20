b9e9ada5aa08aa20f5c74caa6d4950e681091ab2
Chakib BELGANI <chakib.belgani@amadeus.com>
2020-02-20 20:30:32 +0100

    push

--- process.py
+++ process.py
@@ -3,6 +3,13 @@
 from library import Library
 from book import Book
 
+def checklib(l,explored_libs,d):
+    if l.sinupDate <d:
+        for bk in l.unique_books:
+            if not bk.is_scanned:
+                bk.is_scanned=True
+                explored_libs.append(l)
+                
 def process(books, libs, days):
     explored_libs=[]
     startDate=0;
@@ -18,19 +25,18 @@ def process(books, libs, days):
     #Assign to each library a starting date
     for l in libs:
         startDate += l.signup_days
+        if startDate > days:
+            break
         l.sinupDate = startDate
+
 
     #Naive approach
     for d in range(days):
         for l in libs:
-            if l.sinupDate <d:
-                for bk in l.unique_books:
-                    if not bk.is_scanned:
-                        bk.is_scanned=True
-                        explored_libs.append(l)
+            checklib(l,explored_libs,d)
                     
     for l in libs:
         for bk in l.unique_books:
             if bk.is_scanned:
                 print(l.id,bk.id)
-    
+                
